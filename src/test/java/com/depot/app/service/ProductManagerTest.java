98aa209581a8cdd2afccedf6244d918cc16c0b84
StevenNdaye <stevenndaye@gmail.com>
2014-04-19 03:05:34 -0300

    Added a test configuration file

---
+++ src/test/java/com/depot/app/service/ProductManagerTest.java
@@ -0,0 +1,18 @@
+package com.depot.app.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/test/resources/test-service-context.xml")
+public class ProductManagerTest {
+
+
+    @Test
+    public void itShouldAddProduct(){
+
+    }
+
+}
