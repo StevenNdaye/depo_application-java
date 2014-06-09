86d38839d74790fdfeac5bfccbcd168fb57fc2db
StevenNdaye <stevenndaye@gmail.com>
2014-06-08 21:56:07 -0300

    Renamed column user_id to account_id

--- src/main/resources/migration/user_table_creation_roll_forward.sql
+++ src/main/resources/migration/user_table_creation_roll_forward.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS account
 (
-    user_id SERIAL primary key,
+    account_id SERIAL primary key,
     first_name character varying(100) not null,
     last_name character varying(100) not null,
-    username character varying(100) not null,
+    accountname character varying(100) not null,
     password character varying(100) not null,
     email character varying(100) not null,
     phone_number character varying(100) not null,
@@ -12,38 +12,38 @@ CREATE TABLE IF NOT EXISTS account
 
 CREATE TABLE IF NOT EXISTS role
 (
-    user_id integer REFERENCES account(user_id),
+    account_id integer REFERENCES account(account_id),
     role_id integer not null,
     role character varying(100)
 );
 
 MERGE INTO account
     USING (VALUES ('Steven', 'Ndaye', 'StevenNdaye', 'Heloise@87', 'stevenndaye@gmail.com', '+555192281996', true))
-            AS newValues (first_name, last_name, username, password, email, phone_number, active_flag)
-            ON account.username = newValues.username
-    WHEN NOT MATCHED THEN INSERT (first_name, last_name, username, password, email, phone_number, active_flag)
-                          VALUES (newValues.first_name, newValues.last_name, newValues.username, newValues.password, newValues.email,
+            AS newValues (first_name, last_name, accountname, password, email, phone_number, active_flag)
+            ON account.accountname = newValues.accountname
+    WHEN NOT MATCHED THEN INSERT (first_name, last_name, accountname, password, email, phone_number, active_flag)
+                          VALUES (newValues.first_name, newValues.last_name, newValues.accountname, newValues.password, newValues.email,
                           newValues.phone_number, newValues.active_flag);
 
 MERGE INTO role
         USING (VALUES (1, 1, 'admin'))
-        AS newValues (user_id, role_id, role)
-        ON role.user_id = newValues.user_id
-        WHEN NOT MATCHED THEN INSERT (user_id, role_id, role)
-                                VALUES (newValues.user_id, newValues.role_id, newValues.role)
+        AS newValues (account_id, role_id, role)
+        ON role.account_id = newValues.account_id
+        WHEN NOT MATCHED THEN INSERT (account_id, role_id, role)
+                                VALUES (newValues.account_id, newValues.role_id, newValues.role)
 
 MERGE INTO account
     USING (VALUES ('Grace', 'Kibambe', 'GraceKibambe', 'grace', 'gracykib@yahoo.fr', '+27767897061', true))
-            AS newValues (first_name, last_name, username, password, email, phone_number, active_flag)
-            ON account.username = newValues.username
-    WHEN NOT MATCHED THEN INSERT (first_name, last_name, username, password, email, phone_number, active_flag)
-                          VALUES (newValues.first_name, newValues.last_name, newValues.username, newValues.password, newValues.email,
+            AS newValues (first_name, last_name, accountname, password, email, phone_number, active_flag)
+            ON account.accountname = newValues.accountname
+    WHEN NOT MATCHED THEN INSERT (first_name, last_name, accountname, password, email, phone_number, active_flag)
+                          VALUES (newValues.first_name, newValues.last_name, newValues.accountname, newValues.password, newValues.email,
                           newValues.phone_number, newValues.active_flag);
 
 MERGE INTO role
-        USING (VALUES (2, 2, 'user'))
-        AS newValues (user_id, role_id, role)
-        ON role.user_id = newValues.user_id
-        WHEN NOT MATCHED THEN INSERT (user_id, role_id, role)
-                                VALUES (newValues.user_id, newValues.role_id, newValues.role)
+        USING (VALUES (2, 2, 'account'))
+        AS newValues (account_id, role_id, role)
+        ON role.account_id = newValues.account_id
+        WHEN NOT MATCHED THEN INSERT (account_id, role_id, role)
+                                VALUES (newValues.account_id, newValues.role_id, newValues.role)
 
